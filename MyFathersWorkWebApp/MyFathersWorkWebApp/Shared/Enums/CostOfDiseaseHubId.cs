368bc8be76873f8b04c243073fc88260a9d7c0aa
Rocco Manzo <roccomanzo@google.com>
2026-09-22 21:27:17 +0000

    feat(TheCostOfDisease): add Gen 3 state variables, hub IDs, UI extensions, and enums

--- MyFathersWorkWebApp/MyFathersWorkWebApp/Shared/Enums/CostOfDiseaseHubId.cs
+++ MyFathersWorkWebApp/MyFathersWorkWebApp/Shared/Enums/CostOfDiseaseHubId.cs
@@ -1,9 +1,13 @@
-﻿namespace MyFathersWorkWebApp;
+namespace MyFathersWorkWebApp;
 
 public enum CostOfDiseaseHubId
 {
-    None        = 0,
-    Fever       = 1,
-    Devastation = 2,
-    Hospital    = 3
+    None         = 0,
+    Fever        = 1,
+    Devastation  = 2,
+    Hospital     = 3,
+    GloomyGothic = 4,
+    Prosperity   = 5,
+    NoUniversity = 6,
+    University   = 7
 }
